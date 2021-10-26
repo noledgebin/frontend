d168d8c1728ad1b54c78e000500a3bfed25306df
Andinus <andinus@nand.sh>
2021-10-26 17:50:32 +0530

    Add basic send function

--- resources/app.js
+++ resources/app.js
@@ -1,15 +1,13 @@
 'use strict';
-<<<<<<< Updated upstream
+
 function send() {
-    const paste = document.getElementById("user-paste").innerText
-    console.log("User Pasted: " + paste)
+  const paste = document.getElementById("user-paste").innerText;
+  console.log(paste);
+
+  const ciphertext = CryptoJS.AES.encrypt(paste, 'secret-key').toString();
+  console.log(ciphertext);
+
+  let bytes  = CryptoJS.AES.decrypt(ciphertext, 'secret-key');
+  let originalText = bytes.toString(CryptoJS.enc.Utf8);
+  console.log(originalText);
 }
-=======
-
-let ciphertext = CryptoJS.AES.encrypt('my message', 'secret-key').toString();
-console.log(ciphertext);
-
-let bytes  = CryptoJS.AES.decrypt(ciphertext, 'secret-key');
-let originalText = bytes.toString(CryptoJS.enc.Utf8);
-console.log(originalText);
->>>>>>> Stashed changes
