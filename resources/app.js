0d0cda4f6444f49535cf98b8229863cab2b2b3ec
Andinus <andinus@nand.sh>
2021-10-26 23:18:35 +0530

    Add AUTHORS, use a bundler

--- resources/app.js
+++ resources/app.js
@@ -1,13 +1,15 @@
 'use strict';
 
-function send() {
-  const paste = document.getElementById("user-paste").innerText;
-  console.log(paste);
+import * as CryptoJS from 'crypto-js';
 
-  const ciphertext = CryptoJS.AES.encrypt(paste, 'secret-key').toString();
-  console.log(ciphertext);
+window.send = function send() {
+    const paste = document.getElementById("user-paste").innerText;
+    console.log(paste);
 
-  let bytes  = CryptoJS.AES.decrypt(ciphertext, 'secret-key');
-  let originalText = bytes.toString(CryptoJS.enc.Utf8);
-  console.log(originalText);
+    const ciphertext = CryptoJS.AES.encrypt(paste, 'secret-key').toString();
+    console.log(ciphertext);
+
+    let bytes  = CryptoJS.AES.decrypt(ciphertext, 'secret-key');
+    let originalText = bytes.toString(CryptoJS.enc.Utf8);
+    console.log(originalText);
 }
