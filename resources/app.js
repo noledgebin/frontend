b49bb68f63087aa0247464a5cd45a21032b57edf
Pauloper1 <88343134+Pauloper1@users.noreply.github.com>
2021-10-26 20:31:53 +0530

    LZW compression added

--- resources/app.js
+++ resources/app.js
@@ -1,12 +1,15 @@
 'use strict';
-
+import lzwCompress from 'lzwcompress';
 function send() {
   const paste = document.getElementById("user-paste").innerText;
   console.log(paste);
-
-  const ciphertext = CryptoJS.AES.encrypt(paste, 'secret-key').toString();
+  
+  const compressedPaste = lzwCompress.pack(paste);
+  // const original = lzwCompress.unpack(compressedPaste);
+  const ciphertext = CryptoJS.AES.encrypt(compressedPaste, 'secret-key').toString();
   console.log(ciphertext);
-
+  
+  // console.log(original);
   let bytes  = CryptoJS.AES.decrypt(ciphertext, 'secret-key');
   let originalText = bytes.toString(CryptoJS.enc.Utf8);
   console.log(originalText);
