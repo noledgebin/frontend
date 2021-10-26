06f51bad5a5f1bb16475e8e99cd469a6893f59ce
Andinus <andinus@nand.sh>
2021-10-26 17:45:08 +0530

    Import CryptoJS

--- resources/app.js
+++ resources/app.js
@@ -1,5 +1,15 @@
 'use strict';
+<<<<<<< Updated upstream
 function send() {
     const paste = document.getElementById("user-paste").innerText
     console.log("User Pasted: " + paste)
 }
+=======
+
+let ciphertext = CryptoJS.AES.encrypt('my message', 'secret-key').toString();
+console.log(ciphertext);
+
+let bytes  = CryptoJS.AES.decrypt(ciphertext, 'secret-key');
+let originalText = bytes.toString(CryptoJS.enc.Utf8);
+console.log(originalText);
+>>>>>>> Stashed changes
