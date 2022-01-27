d3b13d5a06f09c1d61af09ca7a5617d3b4ec1335
Joel Varghese <joel02varghese@gmail.com>
2022-01-27 14:32:53 +0530

    Connected to backend
    The const ‘frontendOnly’ determines if the app should use the backend or not. If set to true the paste is stored in the URL and so is the passphrase (encryption key). If set to false the paste is sent to the backend which returns a JSON string with an id and deleteId. This id is used to retrieve the paste by sending a GET request and deleteId is used to delete a paste by sending a DELETE request. Deleting a paste has not yet been implemented on the frontend.
    
    ‘sendPostRequest()’ sends a post request to the server with the encrypted text. After sending the request the id is sent and the json is received the id, deleteId and passphrase are sent as parameters to ‘displaySuccessNoteAfterPost()’ which show the user the URL for the paste int the format domain/id#passphrase
    
    ‘getPaste()’ send a get request to the server which sends the paste as a JSON. ‘getPaste()’ then calls ‘displayPaste()’ and passes the encrypted text received from the server and the passphrase (from the fragment identifier of the URL) as parameters. ‘displayPaste()’ then decrypts the paste and shows it to the user.

--- resources/app.js
+++ resources/app.js
@@ -2,11 +2,11 @@
 
 import * as fflate from 'fflate';
 import * as CryptoJS from 'crypto-js';
-import { base64ToUint8Array, uint8ArrayToBase64 } from 'base64-u8array-arraybuffer';
-
+import {base64ToUint8Array, uint8ArrayToBase64} from 'base64-u8array-arraybuffer';
 // cryptoRandomString is async.
 import 'regenerator-runtime/runtime';
 import cryptoRandomString from 'crypto-random-string';
+import {env} from './env.js'
 
 // Paste Elements.
 const pasteBox = document.getElementById('user-paste');
@@ -25,17 +25,51 @@ function displayFailureNote() {
     noteAbovePasteBox.style.display = '';
 }
 
+/**
+ * Displays the paste and url for the paste to the user
+ *
+ * @param id is the id to the paste. Appended to the URL as route
+ * @param deleteId is the deleteId for the paste. Not in use yet
+ * @param passphrase is the decryption key to the paste. Appended to the URL as fragment identifier
+ */
+function displaySuccessNoteAfterPost(id, deleteId, passphrase) {
+    // Add encryptedText and passphrase to fragment url.
+    const pasteUrl = id.concat('#', passphrase);
+    noteAbovePasteBox.innerText = "Your paste is: ";
+
+    // Update the URL to /pasteUrl#passphrase.
+    window.history.pushState("NoledgeBin", "NoledgeBin", `/${pasteUrl}`);
+
+    // Inform user about the Paste.
+    let link = document.createElement('a');
+    link.setAttribute('href', './' + pasteUrl);
+    link.innerText = '/' + pasteUrl;
+
+    noteAbovePasteBox.appendChild(link);
+    displaySuccessNote();
+
+    // Show rendered text.
+    pasteBox.style.display = 'none';
+    pasteBoxRendered.style.display = '';
+    pasteBoxRendered.innerHTML = pasteBox.value;
+}
+
 window.newPaste = function newPaste() {
     // Remove fragment url.
     window.location.hash = '';
+    // Changing route to root this also reloads the page
+    window.location = "/";
 
     // Reload.
-    window.location.reload();
+    // window.location.reload();
 }
 
 window.clonePaste = function clonePaste() {
     // Remove fragment url.
     window.location.hash = '';
+
+    // Update the URL to root.
+    window.history.pushState("NoledgeBin", "NoledgeBin", `/`);
 
     // Clone the paste in pasteBox.
     pasteBox.value = pasteBoxRendered.innerHTML;
@@ -69,9 +103,9 @@ window.sendPaste = function sendPaste() {
             // convert it to string here then JSON.stringify will
             // preserve the Array structure, using much more space.
             paste.text = uint8ArrayToBase64(
-                fflate.compressSync(buf, { level: 6, mem: 8 })
+                fflate.compressSync(buf, {level: 6, mem: 8})
             );
-        } catch(err) {
+        } catch (err) {
             console.log("Decompression failed, turning off.", err);
             paste.text = pasteBox.innerText;
             paste.compressed = false;
@@ -84,32 +118,145 @@ window.sendPaste = function sendPaste() {
         encryptedText = CryptoJS.AES.encrypt(
             JSON.stringify(paste), passphrase
         ).toString();
-    } catch(err) {
+    } catch (err) {
         console.log("Cannot recover from Encryption failure.", err);
         noteAbovePasteBox.innerText = "Encryption failed.";
         displayFailureNote();
         return;
     }
 
-    // Add encryptedText and passphrase to fragment url.
-    const pasteUrl = encryptedText.concat('#', passphrase);
-    noteAbovePasteBox.innerText = "Your paste is: ";
+    if (frontendOnly) {
+        // Add encryptedText and passphrase to fragment url.
+        const pasteUrl = encryptedText.concat('#', passphrase);
+        noteAbovePasteBox.innerText = "Your paste is: ";
 
-    // Update the URL.
-    window.location.hash = pasteUrl;
+        // Update the URL.
+        window.location.hash = pasteUrl;
 
-    // Inform user about the Paste.
-    let link = document.createElement('a');
-    link.setAttribute('href', './#' + pasteUrl);
-    link.innerText = '#' + pasteUrl;
+        // Inform user about the Paste.
+        let link = document.createElement('a');
+        link.setAttribute('href', './#' + pasteUrl);
+        link.innerText = '#' + pasteUrl;
 
-    noteAbovePasteBox.appendChild(link);
-    displaySuccessNote();
+        noteAbovePasteBox.appendChild(link);
+        displaySuccessNote();
 
-    // Show rendered text.
+        // Show rendered text.
+        pasteBox.style.display = 'none';
+        pasteBoxRendered.style.display = '';
+        pasteBoxRendered.innerHTML = pasteBox.value;
+    } else {
+        sendPostRequest(encryptedText, passphrase);
+    }
+}
+
+/**
+ * Decrypts and displays the paste
+ *
+ * @param {string} encryptedText holds the encrypted message
+ * @param {string} passphrase is the decryption key
+ */
+function displayPaste(encryptedText, passphrase) {
+    let paste;
+    try {
+        // Decrypt the text and parse it to get the paste structure.
+        const bytes = CryptoJS.AES.decrypt(encryptedText, passphrase);
+        paste = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+    } catch (err) {
+        console.log("Decryption failure.", err);
+        noteAbovePasteBox.innerText = "Decryption failure.";
+        displayFailureNote();
+        return;
+    }
+
+    if (paste.compressed) {
+        try {
+            // Get the original Uint8Array generated by fflate by
+            // converting paste.text to Array. It was converted to
+            // string so the Array structure was not preserved.
+            const decompressed = fflate.decompressSync(
+                base64ToUint8Array(paste.text)
+            );
+
+            // Store decompressed text.
+            paste.text = fflate.strFromU8(decompressed);
+        } catch (err) {
+            console.log("Decompression failure.", err);
+            noteAbovePasteBox.innerText = "Decompression failure.";
+            displayFailureNote();
+            return;
+        }
+    }
+
+    // Update the text.
     pasteBox.style.display = 'none';
     pasteBoxRendered.style.display = '';
-    pasteBoxRendered.innerHTML = pasteBox.value;
+    pasteBoxRendered.innerHTML = paste.text;
+
+    // Inform user about the paste.
+    noteAbovePasteBox.innerText = "Paste successfully decrypted.";
+    displaySuccessNote();
+}
+
+/**
+ * Sends get request to the server
+ *
+ * @param id is the id of the paste we want to retrieve
+ * @param passphrase is the encryption key for the paste
+ */
+function getPaste(id, passphrase) {
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+            // Creating a object with the received stringifyed JSON
+            const json = JSON.parse(xmlHttp.responseText)
+            displayPaste(json.paste, passphrase);
+        }
+    }
+    xmlHttp.open("GET", `${serverURL}${id}`, true);
+    xmlHttp.send(null);
+}
+
+/**
+ * Sends a post request to the server with the following parameters
+ *
+ * @param {string} encryptedText is the encrypted paste JSON
+ * @param {Number} expiry determines when the paste will be deleted from the server. 0 indicates it will never be deleted
+ * @param {Boolean} burnAfterRead determines if the paste should be deleted after being read once
+ *
+ * @param {string} passphrase is the encryption key to the paste. This is not sent in the post request
+ */
+function sendPostRequest(encryptedText, passphrase, expiry = 0, burnAfterRead = false) {
+    const xmlHttp = new XMLHttpRequest();
+    const url = serverURL;
+    // Creating object with parametes
+    const postObject = {
+        paste: encryptedText,
+        expiry: expiry,
+        burnAfterRead: burnAfterRead
+    }
+    // Creating URLEncoded text with the JSON object
+    const postObjectURLEncoded = new URLSearchParams(postObject).toString();
+    xmlHttp.open('POST', `${url}`, true);
+
+    //Send the proper header information along with the request
+    xmlHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    // Call a function when the state changes.
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+            // Creating a object with the returned text
+            // This object has two attributes:
+            // 1. id
+            // 2. deleteId
+            let pasteObject = JSON.parse(xmlHttp.responseText)
+
+            // Displaying URL and paste to user
+            displaySuccessNoteAfterPost(pasteObject.id, pasteObject.deleteId, passphrase)
+        }
+    }
+    // Sending post request
+    xmlHttp.send(postObjectURLEncoded);
 }
 
 function initialize() {
@@ -117,51 +264,29 @@ function initialize() {
     pasteBox.value = '';
 
     const fragment = window.location.hash.substr(1).split('#');
+    if (frontendOnly) {
+        // Paste & Encryption key in fragment URL.
+        if (fragment.length === 2) {
+            const encryptedText = fragment[0];
+            const passphrase = fragment[1];
 
-    // Paste & Encryption key in fragment URL.
-    if (fragment.length === 2) {
-        const encryptedText = fragment[0];
-        const passphrase = fragment[1];
-
-        let paste;
-        try {
-            // Decrypt the text and parse it to get the paste structure.
-            const bytes = CryptoJS.AES.decrypt(encryptedText, passphrase);
-            paste = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-        } catch(err) {
-            console.log("Decryption failure.", err);
-            noteAbovePasteBox.innerText = "Decryption failure.";
-            displayFailureNote();
-            return;
+            displayPaste(encryptedText, passphrase)
         }
-
-        if (paste.compressed) {
-            try {
-                // Get the original Uint8Array generated by fflate by
-                // converting paste.text to Array. It was converted to
-                // string so the Array structure was not preserved.
-                const decompressed = fflate.decompressSync(
-                    base64ToUint8Array(paste.text)
-                );
-
-                // Store decompressed text.
-                paste.text = fflate.strFromU8(decompressed);
-            } catch(err) {
-                console.log("Decompression failure.", err);
-                noteAbovePasteBox.innerText = "Decompression failure.";
-                displayFailureNote();
-                return;
-            }
+    } else {
+        const route = window.location.pathname.substr(1).split('/');
+        if (route.length === 1 && route[0] !== '' && fragment.length === 1) {
+            const id = route[0];
+            const passphrase = fragment[0];
+            console.log(id)
+            console.log(passphrase)
+            getPaste(id, passphrase);
         }
-
-        // Update the text.
-        pasteBox.style.display = 'none';
-        pasteBoxRendered.style.display = '';
-        pasteBoxRendered.innerHTML = paste.text;
-
-        // Inform user about the paste.
-        noteAbovePasteBox.innerText = "Paste successfully decrypted.";
-        displaySuccessNote();
     }
 }
+
+// Determines if the app is frontend only
+const frontendOnly = false
+// Server url if the app has a backend
+const serverURL = env.serverUrl
+
 initialize();
